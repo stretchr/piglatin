4e7aa4fb6c3d4d15313311dd974f80924ba94107
Mat Ryer <mat@stretchr.com>
2014-03-05 09:41:57 -0700

    initial commit

---
+++ translate.go
@@ -0,0 +1,20 @@
+package piglatin
+
+import (
+	"strings"
+)
+
+const (
+	pigLatinSuffix             string = "ay"
+	firstLetterExceptions      string = "aeiou"
+	firstLetterExceptionSuffix string = "d" + pigLatinSuffix
+)
+
+func Translate(in string) string {
+	first := in[0:1]
+	if strings.Contains(firstLetterExceptions, first) {
+		return in + firstLetterExceptionSuffix
+	} else {
+		return in[1:] + first + pigLatinSuffix
+	}
+}
---
+++ translate_test.go
@@ -0,0 +1,39 @@
+package piglatin
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var translateTests = []struct {
+	in  string
+	out string
+}{
+	{
+		in:  "dog",
+		out: "ogday",
+	},
+	{
+		in:  "cat",
+		out: "atcay",
+	},
+	{
+		in:  "hat",
+		out: "athay",
+	},
+	{
+		in:  "egg",
+		out: "eggday",
+	},
+}
+
+func TestTranslate(t *testing.T) {
+
+	for i, test := range translateTests {
+
+		actual := Translate(test.in)
+		assert.Equal(t, test.out, actual, "Test %d", i)
+
+	}
+
+}
