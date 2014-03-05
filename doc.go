ee6d4c4aa0ed0d487e48b9e262e4a41c48aa6073
Mat Ryer <mat@stretchr.com>
2014-03-05 10:07:24 -0700

    added doc

---
+++ doc.go
@@ -0,0 +1,11 @@
+// Package for translating from English to Pig Latin.
+//
+// Code for TDD article http://blog.stretchr.com/2014/03/05/test-driven-development-specifically-in-golang/
+//
+// Usage
+//
+//     e := "Hello"
+//     p := piglatin.Translate(e)
+//     log.Printf("%s is %s in pig latin.", e, p)
+//
+package piglatin
